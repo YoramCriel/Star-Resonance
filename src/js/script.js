d89c0a49e51cad9108bd58f72d27ad6d97bcd920
YoramCriel <yoram.criel@student.howest.be>
2017-12-09 04:44:09 +0100

    charakter moves

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,9 @@ let app;
 const Application = PIXI.Application,
   loader = PIXI.loader,
   //resources = PIXI.loader.resources,
-  Sprite = PIXI.Sprite;
+  Sprite = PIXI.Sprite,
+  Rectangle = PIXI.Rectangle,
+  TextureCache = PIXI.TextureCache;
 
 const init = () => {
 
@@ -24,8 +26,8 @@ const init = () => {
 
 };
 
+//venster instellen
 const setupPixi = () => {
-
   app = new Application({
     width: window.innerWidth,
     height: window.innerHeight,
@@ -33,10 +35,10 @@ const setupPixi = () => {
     transparent: false,
     resolution: 1
   });
-
   document.body.appendChild(app.view);
 };
 
+// zorgen dat de grote aangepast wordt wanneer het venster in grote veranderd
 const rerenderAppCanvas = () => {
   window.addEventListener(`resize`, function() {
     app.renderer.resize(window.innerWidth, window.innerHeight);
@@ -44,6 +46,7 @@ const rerenderAppCanvas = () => {
 };
 
 
+let charakter;
 
 const loadImages = () => {
   //images laden in de texture cache zodat ze kunnen gebruikt worden met openGL
@@ -51,6 +54,7 @@ const loadImages = () => {
 
   loader
     .add(`assets/img/player.png`)
+    .add(`assets/img/charakter.png`)
     .load(setup)
     .on(`progress`, loadProgressHandler);
 
@@ -59,16 +63,39 @@ const loadImages = () => {
     sprite = new Sprite(
     loader.resources[`assets/img/player.png`].texture
     );
+    sprite.position.set(96, 96);
+    sprite.width = 80;
+    sprite.height = 120;
+    sprite.scale.set(0.5, 0.5);
+    sprite.rotation = 0.5;
+    sprite.anchor.set(0.5, 0.5);
     app.stage.addChild(sprite); // sprite op het scherm zetten
     //app.stage.removeChild(sprite); // sprite verwijderen.
     sprite.visible = true; // omzichtbaar zitten is efficienter
+
+    const texture = TextureCache[`assets/img/charakter.png`];
+    const rectangle = new Rectangle(0, 0, 89, 165);
+    texture.frame = rectangle;
+    charakter = new Sprite(texture);
+    charakter.position.set(500, 500);
+    app.stage.addChild(charakter);
+    app.renderer.render(app.stage);
+    app.ticker.add(delta => gameLoop(delta));
+
   }
 };
 
-const loadProgressHandler = (loader, resource) => {
-  //Display the file `url` currently being loaded
-  console.log(`loading: ${  resource.url}`);
+//gaat ook met requestAnimationFrame(gameLoop);
+const gameLoop = delta => {
+  console.log(delta);
+  //Move the cat 1 pixel
+  charakter.x += 1 + delta;
+};
 
+
+const loadProgressHandler = (loader, resource) => {
+    //Display the file `url` currently being loaded
+  console.log(`loading: ${  resource.url}`);
     //Display the percentage of files currently loaded
   console.log(`progress: ${  loader.progress  }%`);
 
